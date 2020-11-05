0f904cda898ed06f033782bb7d60376d7f9b64e8
luis1 <luis1@DESKTOP-AJ5I6CK>
2020-11-05 15:39:59 -0500

    Version 0

---
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -0,0 +1,18 @@
+
+package cv_ejercicio;
+
+/**
+ *
+ * @author luis1
+ */
+public class Cv_ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Version 0");
+    }
+    
+}
